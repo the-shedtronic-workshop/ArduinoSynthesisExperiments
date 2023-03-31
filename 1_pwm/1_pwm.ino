0e63e3d2164e781f0477c6291a8ff869fe432dad
Shedtronic <38550478+shedtronic@users.noreply.github.com>
2023-03-31 18:24:02 +0100

    Create 1_pwm.ino

---
+++ 1_pwm/1_pwm.ino
@@ -0,0 +1,21 @@
+int speakerPin = 9; // connect speaker to digital pin 9
+
+void setup() {
+
+  pinMode(speakerPin, OUTPUT); // set the speaker pin as an output
+
+}
+
+void loop() {
+
+  // generate a 1 kHz tone with 50% duty cycle (i.e., a 50% PWM signal)
+
+  analogWrite(speakerPin, 127);
+
+  delay(500); // wait for 500 milliseconds
+
+  analogWrite(speakerPin, 0); // turn off the tone
+
+  delay(500); // wait for another 500 milliseconds
+
+}
